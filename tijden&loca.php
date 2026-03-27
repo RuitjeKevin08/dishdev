5dc8f2aaa56ebf1269398f07f3b9bd40a35a0e2e
Ian Both <6056879@mborijnland.nl>
2026-03-27 15:51:26 +0100

    tijden en locatie pagina gemaakt

---
+++ tijden&loca.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Openingstijden</title>
+    <link rel="stylesheet" href="css/ian.css">
+</head>
+<body>
+    <?php include 'header.php'; ?>
+    <main id="tijden-loca-main">
+        <article id="tijden">
+            <img src="img/pannenkoek.png">
+            <h2>Openingstijden</h2>
+            <p>Maandag - 11:00 t/m 21:00</p>
+            <p>Dinsdag - 11:00 t/m 21:00</p>
+            <p>Woensdag - 11:00 t/m 21:00</p>
+            <p>Donderdag - 11:00 t/m 21:00</p>
+            <p>Vrijdag - 11:00 t/m 22:30</p>
+            <p>Zaterdag - 11:00 t/m 22:30</p>
+            <p>Zondag - 11:00 t/m 21:00</p><br>
+        </article>
+        <article id="locatie">
+            <h2>Locatie</h2>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2435.702670331915!2d4.881845476338234!3d52.37581114689537!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c609c517d5357b%3A0x8651ac5b339f737!2sPrinsengracht%20245%2C%20Amsterdam!5e0!3m2!1snl!2snl!4v1774607145716!5m2!1snl!2snl"allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <p>
+                Prinsengracht 245<br>
+                1016 GV Amsterdam<br>
+                Noord-Holland<br>
+                Nederland
+            </p>
+        </article>
+        <article id="tijden-vakantie">
+            <img src="img/stamppot.png">
+            <h2>Vakantie openingstijden</h2>
+            <p>Maandag - 11:00 t/m 23:00</p>
+            <p>Dinsdag - 11:00 t/m 23:00</p>
+            <p>Woensdag - 11:00 t/m 23:00</p>
+            <p>Donderdag - 11:00 t/m 23:00</p>
+            <p>Vrijdag - 11:00 t/m 23:30</p>
+            <p>Zaterdag - 10:00 t/m 23:30</p>
+            <p>Zondag - 10:00 t/m 23:00</p>
+        </article>
+    </main>
+</body>
+</html>
