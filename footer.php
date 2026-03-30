75bfb48ea2274dd671629b057d11595d677626c3
Ian Both <6056879@mborijnland.nl>
2026-03-30 13:21:11 +0200

    footer gemaakt en extras gefixt

---
+++ footer.php
@@ -0,0 +1,22 @@
+<footer>
+    <p>
+        Prinsengracht 245<br>
+        1016 GV Amsterdam<br>
+        Noord-Holland<br>
+        Nederland<br>
+        omas-kost@gmail.com<br>
+        +31 6 676767<br>
+        alle rechten voorbehouden
+    </p>
+</footer>
+<footer id="footer-tablet-desktop">
+    <p>
+        Prinsengracht 245 
+        1016 GV Amsterdam, 
+        Noord-Holland 
+        Nederland, 
+        omas-kost@gmail.com, 
+        +31 6 676767, 
+        alle rechten voorbehouden
+    </p>
+</footer>
