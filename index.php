b8cac52208702ad3f87f5e7306ba682c1b91ef2d
Blevin <146931310+RuitjeKevin08@users.noreply.github.com>
2026-04-05 22:24:18 +0200

    dishdev is af

--- index.php
+++ index.php
@@ -9,22 +9,59 @@
 <body>
     <?php include 'header.php'; ?>
    <section id="images" class="m-home-container">
-    <article class="boven">
+        <article class="boven">
         <article class="fotom-boven"><img src="img/rookworst.png"></article>
-        <h1 class="h1m-boven">Beste stamppot</h1>
-        <p class="pm-boven">Bij Oma's Kost staat boerenkool stamppot op het menu als echte Hollandse klassieker. De boerenkool wordt gemengd met aardappelen tot een stevige stamppot en geserveerd met rookworst of spekjes. Het gerecht is warm, vullend en vooral populair in de winter.</p>
-    </article>
-    <article class="midden">
+            <h1 class="h1m-boven">Beste stamppot</h1>
+            <p class="pm-boven">Bij Oma's Kost staat boerenkool stamppot op het menu als echte Hollandse klassieker. De boerenkool wordt gemengd met aardappelen tot een stevige stamppot en geserveerd met rookworst of spekjes. Het gerecht is warm, vullend en vooral populair in de winter.</p>
+        </article>
+        <article class="midden">
         <article class="fotom-midden"><img src="img/stamppot.png"></article>
-        <h1 class="h1m-midden">Hutspot</h1>
-        <p class="pm-midden">Bij Oma's Kost is hutspot een traditioneel gerecht dat vaak wordt besteld. Het wordt gemaakt van aardappelen, wortels en uien die samen worden gekookt en daarna gestampt. Meestal wordt het geserveerd met een stuk vlees, zoals klapstuk of rookworst. Het is een stevige  en warme maaltijd.</p>
-    </article>
-    <article class="onder">
+            <h1 class="h1m-midden">Hutspot</h1>
+            <p class="pm-midden">Bij Oma's Kost is hutspot een traditioneel gerecht dat vaak wordt besteld. Het wordt gemaakt van aardappelen, wortels en uien die samen worden gekookt en daarna gestampt. Meestal wordt het geserveerd met een stuk vlees, zoals klapstuk of rookworst. Het is een stevige  en warme maaltijd.</p>
+        </article>
+        <article class="onder">
         <article class="fotom-onder"><img src="img/pannenkoek.png"></article>
-        <h1 class="h1m-onder">Pannenkoek</h1>
-        <p class="pm-onder">Bij Oma's Kost staan ook pannenkoeken op het menu. Ze worden vers gebakken en kunnen zowel zoet als hartig worden besteld. Denk aan pannenkoeken met stroop of poedersuiker, maar ook met spek of kaas. Het is een eenvoudig gerecht dat bij veel gasten in de smaak valt.</p>
-    </article>
-   </section>
+            <h1 class="h1m-onder">Pannenkoek</h1>
+            <p class="pm-onder">Bij Oma's Kost staan ook pannenkoeken op het menu. Ze worden vers gebakken en kunnen zowel zoet als hartig worden besteld. Denk aan pannenkoeken met stroop of poedersuiker, maar ook met spek of kaas. Het is een eenvoudig gerecht dat bij veel gasten in de smaak valt.</p>
+        </article>
+    </section>
+    <section class="t-home-container-b">
+        <article class="t-links-boven">
+            <img class="t-foto" src="img/rookworst.png" alt="foto van rookworst">
+            <h1 class="t-h1">Beste stamppot</h1>
+            <p class="t-p">Oma's stamppot is het beste dat er is! Of je nou langs komt vanaf een lange dag werken, of als je gewoon een keertje langs wil komen, Oma's stamppot is altijd lekker om op te smikkelen.</p>
+        </article>
+        <article class="t-rechts-boven">
+            <img class="t-foto" src="img/stamppot.png" alt="foto van stamppot">
+            <h1 class="t-h1">Hutspot</h1>
+            <p class="t-p">Oma's hutspot is een gerecht wat je niet kan laten liggen. Dit komt niet alleen door onze verse wortels die door het gerecht gaan of onze zelf gegroeide aardappelen en uien, maar ook door het fantastische gehakt wat door dit ongeloflijk lekkere gerecht heen zitten</p>
+        </article>
+    </section>
+    <section class="t-home-container-o">
+        <article class="t-links-onder">
+            <h1 class="t-h1">Over oma's kost</h1>
+        </article>
+        <article class="t-rechts-onder">
+            <p class="t-p-onder">Oma's kost is gemaakt omdat we wilden dat mensen dingen konden eten die ze vroeger altijd bij oma te eten kregen. Dit stukje nostalgie willen wij graag vastleggen door middel van ons eten.</p>
+        </article>
+    </section>
+    <section class="d-home-container">
+        <article class="d-h-links">
+            <img class="d-foto" src="img/rookworst.png" alt="foto van rookworst">
+            <h1 class="d-h1">Beste stamppot</h1>
+            <p class="d-p">Oma's stamppot is het beste dat er is! Of je nou langs komt vanaf een lange dag werken, of als je gewoon een keertje langs wil komen, Oma's stamppot is altijd lekker om op te smikkelen.</p>
+        </article>
+        <article class="d-h-midden">
+            <img class="d-foto" src="img/stamppot.png" alt="foto van stamppot">
+            <h1 class="d-h1">Hutspot</h1>
+            <p class="d-p">Oma's hutspot is een gerecht wat je niet kan laten liggen. Dit komt niet alleen door onze verse wortels die door het gerecht gaan of onze zelf gegroeide aardappelen en uien, maar ook door het fantastische gehakt wat door dit ongeloflijk lekkere gerecht heen zitten</p>
+        </article>
+        <article class="d-h-rechts">
+            <img class="d-foto" src="img/pannenkoek.png" alt="foto van pannenkoek">
+            <h1 class="d-h1">Over oma's kost</h1>
+            <p class="d-p">Oma's kost is gemaakt omdat we wilden dat mensen dingen konden eten die ze vroeger altijd bij oma te eten kregen. Dit stukje nostalgie willen wij graag vastleggen door middel van ons eten.</p>
+        </article>
+    </section>
     <?php include 'footer.php'; ?>
 </body>
 </html>
